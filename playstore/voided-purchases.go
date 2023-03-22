8adee4963e69b209159b3a4f5fafa5569ebc3a04
coolishbee <chc3484@gmail.com>
2023-03-22 17:30:33 +0900

    [add] voided purchases api

---
+++ playstore/voided-purchases.go
@@ -0,0 +1,50 @@
+package playstore
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"google.golang.org/api/option"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	androidpublisher "google.golang.org/api/androidpublisher/v3"
+)
+
+type Client struct {
+	service *androidpublisher.Service
+}
+
+func New(jsonKey []byte) (*Client, error) {
+	c := &http.Client{Timeout: 10 * time.Second}
+	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, c)
+
+	conf, err := google.JWTConfigFromJSON(jsonKey, androidpublisher.AndroidpublisherScope)
+	if err != nil {
+		return nil, err
+	}
+
+	val := conf.Client(ctx).Transport.(*oauth2.Transport)
+	_, err = val.Source.Token()
+	if err != nil {
+		return nil, err
+	}
+
+	service, err := androidpublisher.NewService(ctx, option.WithHTTPClient(conf.Client(ctx)))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{service}, err
+}
+
+func (c *Client) VoidedList(
+	ctx context.Context,
+	packageName string,
+) (*androidpublisher.VoidedPurchasesListResponse, error) {
+	ps := androidpublisher.NewPurchasesVoidedpurchasesService(c.service)
+	result, err := ps.List(packageName).Context(ctx).Do()
+
+	return result, err
+}
---
+++ playstore/voided-purchases_test.go
@@ -0,0 +1,48 @@
+package playstore
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestVoidedList(t *testing.T) {
+	ctx := context.Background()
+
+	jsonFile, err := os.ReadFile("service-account.json")
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	client, err := New(jsonFile)
+	if err != nil {
+		t.Errorf("got %#v", err)
+	}
+	result, err := client.VoidedList(ctx, "packageName")
+	if err != nil {
+		t.Errorf("got %v", err)
+	}
+	if result != nil {
+		if result.TokenPagination != nil {
+			fmt.Println(result.TokenPagination.NextPageToken)
+		} else {
+			fmt.Println("result.TokenPagination nil")
+		}
+		if result.VoidedPurchases != nil {
+			for _, item := range result.VoidedPurchases {
+				fmt.Println(item.PurchaseToken)
+				fmt.Println(item.PurchaseTimeMillis)
+				fmt.Println(item.VoidedTimeMillis)
+				fmt.Println(item.OrderId)
+				fmt.Println(item.VoidedSource)
+				fmt.Println(item.VoidedReason)
+				fmt.Println(item.Kind)
+			}
+		} else {
+			fmt.Println("result.VoidedPurchases nil")
+		}
+	} else {
+		fmt.Println("result nil")
+	}
+}
